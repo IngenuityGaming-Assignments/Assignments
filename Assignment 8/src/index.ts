d35f0afdfd4a4657a10959336f11eb63ee37ce37
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2021-11-19 13:45:07 +0530

    Assignment 8

--- Assignment 8/src/index.ts
+++ Assignment 8/src/index.ts
@@ -1,22 +1,20 @@
 import {Weather} from './api'
 
-//delhi
-//chandigarh
-// amritsar
-// palwal
-// faridbad
-//noida
-// const wea = new Weather("noida");
+// properties
+const humidity = <HTMLSpanElement>document.getElementById('humidity');
+const visibility = <HTMLSpanElement>document.getElementById('visibilty');
+const pressure = <HTMLSpanElement>document.getElementById('pressure')
+const uv = <HTMLSpanElement>document.getElementById('uv')
+const cur_temp = <HTMLSpanElement>document.getElementById('cur_temp');
+const feels_like = <HTMLSpanElement>document.getElementById('feels_like');
 
-//     wea.getDataa().then((d) =>{
-//       console.log(d)
-//     }).catch(e => {
-//         console.log(e)
-//     })
+const cloudy = document.getElementById('cloudy');
+const sun_cloud = document.getElementById('sun_cloud')
+const clear = document.getElementById('clear')
 
 // locations
 const faridabad = <HTMLButtonElement>document.getElementById('faridabad');
-const mumbai = <HTMLButtonElement>document.getElementById('delhi');
+const mumbai = <HTMLButtonElement>document.getElementById('mumbai');
 const noida = <HTMLButtonElement>document.getElementById('noida');
 const palwal = <HTMLButtonElement>document.getElementById('palwal');
 const amritsar = <HTMLButtonElement>document.getElementById('amritsar');
@@ -30,32 +28,18 @@ noida.addEventListener('click', getWeather)
 palwal.addEventListener('click', getWeather)
 amritsar.addEventListener('click', getWeather)
 
-// properties
-const humidity = <HTMLSpanElement>document.getElementById('humidity');
-const visibility = <HTMLSpanElement>document.getElementById('visibilty');
-const pressure = <HTMLSpanElement>document.getElementById('pressure')
-const uv = <HTMLSpanElement>document.getElementById('uv')
-const cur_temp = <HTMLSpanElement>document.getElementById('cur_temp');
-const feels_like = <HTMLSpanElement>document.getElementById('feels_like');
 
-const cloudy = document.getElementById('cloudy');
-const sun_cloud = document.getElementById('sun_cloud')
-const clear = document.getElementById('clear')
 
 function getWeather(){
     location.innerText = this.innerText
     fetchApi(this.innerText);
 }
 
-window.onload = function(){
-    fetchApi("Palwal")
-}
-
 async function fetchApi(t:string) {
     const wea = new Weather(t);
 
     const info = await wea.getDataa().then((d) =>{
-        console.log(d.current)
+        // console.log(d.current)
         return d.current
     }).catch(e => {
         console.log(e)
@@ -87,5 +71,6 @@ async function fetchApi(t:string) {
     }
 }
     
-
-
+window.onload = function(){
+    fetchApi("Palwal")
+}
